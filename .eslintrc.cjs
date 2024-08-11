5d92b78acef5af43e8602cf37cfc6297782ff208
Thai <tranthai.070104@gmail.com>
2024-08-11 13:05:30 +0700

    add cssBaseline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,6 +16,13 @@ module.exports = {
     'react/prop-types': 0,
     'react/display-name': 0,
 
+    'no-restricted-imports': [
+      "error",
+      {
+        "patterns": ["@mui/*/*/*"]
+      }
+    ],
+
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
