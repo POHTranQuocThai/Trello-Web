179ce41b9ee02cdff22dd8672a41f0e576a31d25
Thai <tranthai.070104@gmail.com>
2024-08-12 18:44:34 +0700

    dark_light and Layout

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,25 @@
-import { deepOrange, orange, teal, cyan } from "@mui/material/colors";
-import { createTheme } from "@mui/material/styles";
-import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import { deepOrange, orange, teal, cyan } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
-        secondary: deepOrange,
-      },
+        secondary: deepOrange
+      }
     },
     dark: {
       palette: {
         primary: cyan,
-        secondary: orange,
-      },
-    },
-  },
-});
+        secondary: orange
+      }
+    }
+  }
+})
 
-export default theme;
+export default theme
