5f4a0b3bf5a90c03634cfdc9706192d4025d13bc
Thai <tranthai.070104@gmail.com>
2024-08-13 10:22:34 +0700

    AppBar p1

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,19 +1,55 @@
-import { Box } from '@mui/material'
-import ModeSelect from '../ModeSelect'
+import { Avatar, Badge, Box, Button, IconButton, TextField, Tooltip, Typography } from '@mui/material'
+import ModeSelect from '~/components/ModeSelect'
+import AppsIcon from '@mui/icons-material/Apps'
+import { ReactComponent as TrelloIcon } from '~/assets/trello.svg'
+import SvgIcon from '@mui/material/SvgIcon'
+import Workspaces from './Menus/Workspaces'
+import Recent from './Menus/Recent'
+import Starred from './Menus/Starred'
+import Templates from './Menus/Templates'
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications'
+import NotListedLocationIcon from '@mui/icons-material/NotListedLocation'
+import Profiles from './Menus/Profiles'
+
 
 function AppBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.light',
         width: '100%',
         height: (theme) => theme.trello.appBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingX: 1
       }}
     >
-      <ModeSelect />
-    </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <AppsIcon sx={{ color: 'primary.main' }} />
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+          <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>Trello</Typography>
+        </Box>
+        <Workspaces />
+        <Recent />
+        <Starred />
+        <Templates />
+        <Button variant="outlined">Create</Button>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <TextField id="outlined-search" label="Search..." type="search" size='small' />
+        <ModeSelect />
+        <Tooltip title='Notification' >
+          <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
+            <CircleNotificationsIcon />
+          </Badge>
+        </Tooltip>
+        <Tooltip title='Help'>
+          <NotListedLocationIcon sx={{ cursor: 'pointer' }} />
+        </Tooltip>
+        <Profiles />
+      </Box>
+    </Box >
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteSvgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSvgr()
+  ],
   // base: './'
+  resolve: {
+    alias: [
+      { find: '~', replacement: '/src' }
+    ]
+  }
 })
