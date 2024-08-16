ca58820ae3588df5127e66fed5ab6fe00733ed8c
Thai <tranthai.070104@gmail.com>
2024-08-16 13:53:51 +0700

    keo tha card

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -19,12 +19,14 @@ import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 
 function Column({ column }) {
-  const { attributes, listeners, setNodeRef, transform, transition
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging
   } = useSortable({ id: column._id, data: { ...column } })
 
   const dndKitColumnStyles = {
-    toucchAction: 'none',
-    transform: CSS.Translate.toString(transform), transition
+    //toucchAction: 'none', //danh cho con tro
+    transform: CSS.Translate.toString(transform), transition,
+    height: '100%', //tránh lỗi giật giật khi kéo
+    opacity: isDragging ? 0.5 : undefined
   }
 
   const [anchorEl, setAnchorEl] = useState(null)
@@ -37,129 +39,131 @@ function Column({ column }) {
   }
   const orderCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
   return (
-    <Box
-      ref={setNodeRef}
+    <div ref={setNodeRef}
       style={dndKitColumnStyles}
-      {...attributes} {...listeners}
-      sx={{
-        minWidth: '300px',
-        maxWidth: '300px',
-        bgcolor: (theme) =>
-          theme.palette.mode === 'dark' ? '#333643' : '#ebecf0',
-        ml: 2,
-        borderRadius: '6px',
-        height: 'fit-content',
-        maxHeight: (theme) =>
-          `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
-      }}
-    >
+      {...attributes}>
       <Box
+        {...listeners}
         sx={{
-          height: (theme) => theme.trello.column_header_height,
-          p: 2,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between'
+          minWidth: '300px',
+          maxWidth: '300px',
+          bgcolor: (theme) =>
+            theme.palette.mode === 'dark' ? '#333643' : '#ebecf0',
+          ml: 2,
+          borderRadius: '6px',
+          height: 'fit-content',
+          maxHeight: (theme) =>
+            `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`
         }}
       >
-        <Typography
-          variant="h6"
+        <Box
           sx={{
-            fontSize: '1rem',
-            fontWeight: 'bold',
-            cursor: 'pointer'
+            height: (theme) => theme.trello.column_header_height,
+            p: 2,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
           }}
         >
-          {column?.title}
-        </Typography>
-        <Box>
-          <Tooltip title="More Options">
-            <ExpandMoreIcon
-              sx={{ color: 'text.primary', cursor: 'pointer' }}
-              id="basic-button-dropdown"
-              aria-controls={open ? 'basic-menu-dropdown' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-            />
-          </Tooltip>
-          <Menu
-            id="basic-menu-dropdown"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button-dropdown'
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: '1rem',
+              fontWeight: 'bold',
+              cursor: 'pointer'
             }}
           >
-            <MenuList>
-              <MenuItem>
-                <ListItemIcon>
-                  <AddCardIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Add New Card</ListItemText>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <ContentCut fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Cut</ListItemText>
-                <Typography variant="body2" color="text.secondary">
-                  ⌘X
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <ContentCopy fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Copy</ListItemText>
-                <Typography variant="body2" color="text.secondary">
-                  ⌘C
-                </Typography>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <ContentPaste fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Paste</ListItemText>
-                <Typography variant="body2" color="text.secondary">
-                  ⌘V
-                </Typography>
-              </MenuItem>
-              <Divider />
-              <MenuItem>
-                <ListItemIcon>
-                  <DeleteForeverIcon fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Delete this column</ListItemText>
-              </MenuItem>
-              <MenuItem>
-                <ListItemIcon>
-                  <Cloud fontSize="small" />
-                </ListItemIcon>
-                <ListItemText>Archive this column</ListItemText>
-              </MenuItem>
-            </MenuList>
-          </Menu>
+            {column?.title}
+          </Typography>
+          <Box>
+            <Tooltip title="More Options">
+              <ExpandMoreIcon
+                sx={{ color: 'text.primary', cursor: 'pointer' }}
+                id="basic-button-dropdown"
+                aria-controls={open ? 'basic-menu-dropdown' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+              />
+            </Tooltip>
+            <Menu
+              id="basic-menu-dropdown"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button-dropdown'
+              }}
+            >
+              <MenuList>
+                <MenuItem>
+                  <ListItemIcon>
+                    <AddCardIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Add New Card</ListItemText>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon>
+                    <ContentCut fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Cut</ListItemText>
+                  <Typography variant="body2" color="text.secondary">
+                    ⌘X
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon>
+                    <ContentCopy fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Copy</ListItemText>
+                  <Typography variant="body2" color="text.secondary">
+                    ⌘C
+                  </Typography>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon>
+                    <ContentPaste fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Paste</ListItemText>
+                  <Typography variant="body2" color="text.secondary">
+                    ⌘V
+                  </Typography>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <DeleteForeverIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Delete this column</ListItemText>
+                </MenuItem>
+                <MenuItem>
+                  <ListItemIcon>
+                    <Cloud fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Archive this column</ListItemText>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+        <ListCards cards={orderCards} />
+        {/* card footer */}
+        <Box
+          sx={{
+            height: (theme) => theme.trello.column_footer_height,
+            p: 2,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Button startIcon={<AddCardIcon />}>Add New Card</Button>
+          <Tooltip title="Drag to move">
+            <DragHandleIcon sx={{ cursor: 'pointer' }} />
+          </Tooltip>
         </Box>
       </Box>
-      <ListCards cards={orderCards} />
-      {/* card footer */}
-      <Box
-        sx={{
-          height: (theme) => theme.trello.column_footer_height,
-          p: 2,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between'
-        }}
-      >
-        <Button startIcon={<AddCardIcon />}>Add New Card</Button>
-        <Tooltip title="Drag to move">
-          <DragHandleIcon sx={{ cursor: 'pointer' }} />
-        </Tooltip>
-      </Box>
-    </Box>
+    </div>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -1,57 +1,70 @@
 import {
-    Button,
-    CardActions,
-    CardContent,
-    CardMedia,
-    Typography
+  Button,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography
 } from '@mui/material'
 import { Card as MuiCard } from '@mui/material'
 import GroupIcon from '@mui/icons-material/Group'
 import CommentIcon from '@mui/icons-material/Comment'
 import AttachmentIcon from '@mui/icons-material/Attachment'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 function Card({ card }) {
 
-    const shouldShowCardAction = () => {
-        return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
-    }
-    return (
-        <MuiCard
-            sx={{
-                cursor: 'pointer',
-                boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
-                overflow: 'unset'
-            }}
-        >
-            {card?.cover && (
-                <CardMedia sx={{ height: 140 }} image={card?.cover} title="" />
-            )}
-            <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
-                <Typography>{card?.title}</Typography>
-            </CardContent>
-            {
-                shouldShowCardAction() &&
-                <CardActions sx={{ p: '0 4px 8px 4px' }}>
-                    {!!card?.memberIds?.length && (
-                        <Button size="small" startIcon={<GroupIcon />}>
-                            {card?.memberIds?.length}
-                        </Button>
-                    )}
-                    {!!card?.comments?.length && (
-                        <Button size="small" startIcon={<CommentIcon />}>
-                            {card?.comments?.length}
-                        </Button>
-                    )}
-                    {!!card?.attachments?.length && (
-                        <Button size="small" startIcon={<AttachmentIcon />}>
-                            {card?.attachments?.length}
-                        </Button>
-                    )}
-                </CardActions>
-            }
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging
+  } = useSortable({ id: card._id, data: { ...card } })
 
-        </MuiCard>
-    )
+  const dndKitCardStyles = {
+    //toucchAction: 'none', //danh cho con tro
+    transform: CSS.Translate.toString(transform), transition,
+    opacity: isDragging ? 0.5 : undefined
+  }
+
+  const shouldShowCardAction = () => {
+    return !!card?.memberIds?.length || !!card?.comments?.length || !!card?.attachments?.length
+  }
+  return (
+    <MuiCard ref={setNodeRef}
+      style={dndKitCardStyles}
+      {...attributes} {...listeners}
+      sx={{
+        cursor: 'pointer',
+        boxShadow: '0 1px 1px rgba(0,0,0,0.2)',
+        overflow: 'unset'
+      }}
+    >
+      {card?.cover && (
+        <CardMedia sx={{ height: 140 }} image={card?.cover} title="" />
+      )}
+      <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+        <Typography>{card?.title}</Typography>
+      </CardContent>
+      {
+        shouldShowCardAction() &&
+        <CardActions sx={{ p: '0 4px 8px 4px' }}>
+          {!!card?.memberIds?.length && (
+            <Button size="small" startIcon={<GroupIcon />}>
+              {card?.memberIds?.length}
+            </Button>
+          )}
+          {!!card?.comments?.length && (
+            <Button size="small" startIcon={<CommentIcon />}>
+              {card?.comments?.length}
+            </Button>
+          )}
+          {!!card?.attachments?.length && (
+            <Button size="small" startIcon={<AttachmentIcon />}>
+              {card?.attachments?.length}
+            </Button>
+          )}
+        </CardActions>
+      }
+
+    </MuiCard>
+  )
 }
 
 export default Card
