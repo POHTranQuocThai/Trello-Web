46c8fd5b6249a5603f192761e31ca5a63ad934e0
Thai <tranthai.070104@gmail.com>
2024-08-17 16:58:41 +0700

    card in column final

--- src/utils/sorts.js
+++ src/utils/sorts.js
@@ -1,5 +1,5 @@
 
 export const mapOrder = (originalArray, orderArray, key) => {
-    if (!originalArray || !orderArray || !key) return []
-    return [...originalArray].sort((a, b) => orderArray.indexOf(a[key]) - orderArray.indexOf(b[key]))
+  if (!originalArray || !orderArray || !key) return []
+  return [...originalArray].sort((a, b) => orderArray.indexOf(a[key]) - orderArray.indexOf(b[key]))
 }
