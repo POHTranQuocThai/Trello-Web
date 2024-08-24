8868dbdcc9fae2d2207e9ad39ae008a60a71be5a
Thai <tranthai.070104@gmail.com>
2024-08-24 16:34:41 +0700

    delete col card

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -7,7 +7,7 @@ import CloseIcon from '@mui/icons-material/Close'
 import { toast } from 'react-toastify'
 
 
-function ListColumns({ columns, createNewColumn, createNewCard }) {
+function ListColumns({ columns, createNewColumn, createNewCard, deleteColumnDetails }) {
   const [openNewColumnForm, setOpenNewColumnForm] = useState(false)
   const toggleOpenNewColumnForm = () => setOpenNewColumnForm(!openNewColumnForm)
   const [newColumnTitle, setNewColumnTitle] = useState('')
@@ -37,7 +37,7 @@ function ListColumns({ columns, createNewColumn, createNewCard }) {
           '&::-webkit-scrollbar-track': { m: 2 }
         }}
       >
-        {columns?.map(column => <Column key={column._id} column={column} createNewCard={createNewCard} />)}
+        {columns?.map(column => <Column key={column._id} column={column} createNewCard={createNewCard} deleteColumnDetails={deleteColumnDetails} />)}
 
         {/* Box Add New Column */}
         {!openNewColumnForm ?
