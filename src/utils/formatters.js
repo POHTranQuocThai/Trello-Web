662ec4e5d08c24561ba47318d098fa1b92422780
POHTranQuocThai <tranthai.070104@gmail.com>
2024-10-24 19:52:22 +0700

    delete comment

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -1,10 +1,4 @@
-/**
- * YouTube: TrungQuanDev - Một Lập Trình Viên
- * Created by trungquandev.com's author on Jun 28, 2023
- */
-/**
- * Capitalize the first letter of a string
- */
+
 export const capitalizeFirstLetter = (val) => {
   if (!val) return ''
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
@@ -23,9 +17,3 @@ export const generatePlaceholderCard = (column) => {
  */
 
 
-/**
- * Results:
- *
- * stringTest: Of course, nothing changes =))
- * capString: Trungquandev
- */
