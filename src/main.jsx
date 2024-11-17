32f0fdd5e0b414d9e572b60a9a63a98bf9f8f7fd
POHTranQuocThai <tranthai.070104@gmail.com>
2024-11-17 22:52:16 +0700

    cấu hình redux,redux toolkit, thunkMiddleware

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,22 @@ import theme from './theme.js'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { ConfirmProvider } from 'material-ui-confirm'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <CssVarsProvider theme={theme}>
-    <ConfirmProvider defaultOptions={{
-      allowClose: false,
-      dialogProps: { maxWidth: 'xs' },
-      confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
-      cancellationButtonProps: { color: 'inherit' }
-    }}>
-      <CssBaseline />
-      <App />
-      <ToastContainer theme="colored" position="bottom-left" />
-    </ConfirmProvider>
-  </CssVarsProvider>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      <ConfirmProvider defaultOptions={{
+        allowClose: false,
+        dialogProps: { maxWidth: 'xs' },
+        confirmationButtonProps: { color: 'secondary', variant: 'outlined' },
+        cancellationButtonProps: { color: 'inherit' }
+      }}>
+        <CssBaseline />
+        <App />
+        <ToastContainer theme="colored" position="bottom-left" />
+      </ConfirmProvider>
+    </CssVarsProvider>
+  </Provider>
 )
---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { activeBoardReducer } from "./activeBoard/activeBoardSlice";
+
+
+export const store = configureStore({
+    reducer: {
+        activeBoard: activeBoardReducer
+    },
+})
